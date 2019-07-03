642f1d21b520dc6f04fc7b4b1a5c166d977808db
Xiang Zhou <1041287344@qq.com>
2019-07-03 15:44:20 +0800

    controller 新增常量

---
+++ common-web/common-web-controller/src/main/java/com/faxe/web/controller/util/Constant.java
@@ -0,0 +1,43 @@
+package com.faxe.web.controller.util;
+
+/**
+ * @author Xiang Zhou
+ * @create 2018-08-28 16:36
+ **/
+public class Constant {
+
+    /**
+     * 操作
+     */
+    public static class Action {
+        public static final String SAVE = "commons.web.action.save";
+        public static final String UPDATE = "commons.web.action.update";
+        public static final String COMMAND = "commons.web.action.command";
+        public static final String MERGE = "commons.web.action.merge";
+        public static final String QUERY = "commons.web.action.query";
+        public static final String DELETE = "commons.web.action.delete";
+        public static final String UPLOAD = "commons.web.action.upload";
+        public static final String DOWNLOAD = "commons.web.action.download";
+        public static final String LOGIN = "commons.web.action.login";
+        public static final String LOGOUT = "commons.web.action.logout";
+        public static final String IMPORT = "commons.web.action.import";
+        public static final String EXPORT = "commons.web.action.export";
+    }
+
+    /**
+     * 操作
+     */
+    public static class Resource {
+        public static final String SAVE = "SAVE";
+        public static final String UPDATE = "UPDATE";
+        public static final String COMMAND = "UPDATE";
+        public static final String MERGE = "UPDATE";
+        public static final String QUERY = "QUERY";
+        public static final String DELETE = "DELETE";
+        public static final String UPLOAD = "UPLOAD";
+        public static final String DOWNLOAD = "DOWNLOAD";
+        public static final String IMPORT = "IMPORT";
+        public static final String EXPORT = "EXPORT";
+    }
+
+}
